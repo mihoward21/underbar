e0dec3f51fdca6673e0d9ecc3f50d9aa841f48c2
Mike <mikeh09@gmail.com>
2014-08-01 15:35:22 -0400

    _.reduce() passes all the tests

--- src/underbar.js
+++ src/underbar.js
@@ -190,6 +190,17 @@ var _ = {};
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
+  	if(arguments.length > 2){
+  		var total = accumulator;
+  	}
+  	else {
+  		var total = collection[0];
+  	}
+  	_.each(collection, function(value, key){
+  		total = iterator(total,value);
+  	});
+  	
+  	return total;
   };
 
   // Determine if the array or object contains a given value (using `===`).
