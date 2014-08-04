1d05e3bde8a19a4825bea24e7a81e871fb7f224d
Mike <mikeh09@gmail.com>
2014-08-04 17:57:03 -0400

    _.delay() passes all the tests, seems a little long/ugly though

--- src/underbar.js
+++ src/underbar.js
@@ -364,6 +364,17 @@ var _ = {};
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+  	var help = [];
+  	//arguments.slice(2,arguments.length);
+  	for(var i = 2; i < arguments.length; i++){
+  		help.push(arguments[i]);
+  	}
+  	if(help.length === 0){
+  		setTimeout(func,wait);
+  	}
+  	else{
+  		setTimeout(func.apply(this, help),wait);
+  	}
   };
 
 
